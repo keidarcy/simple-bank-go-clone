6678ec99a5e7b27d5f2ababb9e5e963a8a87bde8
Xing Yahao <keidarcy.1015@gmail.com>
2023-05-27 22:25:27 +0900

    chore: grpc gateway

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"log"
 	"net"
+	"net/http"
 
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"github.com/keidarcy/simple-bank/api"
 	db "github.com/keidarcy/simple-bank/db/sqlc"
 	"github.com/keidarcy/simple-bank/gapi"
@@ -13,6 +16,7 @@ import (
 	_ "github.com/lib/pq"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/encoding/protojson"
 )
 
 func main() {
@@ -30,7 +34,8 @@ func main() {
 
 	store := db.NewStore(conn)
 	// runGinServer(config, store)
-	runGrpcServer(config, store)
+	go runGrpcServer(config, store)
+	runGatewayServer(config, store)
 }
 
 func runGrpcServer(config util.Config, store db.Store) {
@@ -51,6 +56,42 @@ func runGrpcServer(config util.Config, store db.Store) {
 	err = grpcServer.Serve(listener)
 	if err != nil {
 		log.Fatal("cannot start gPRC server")
+	}
+
+}
+
+func runGatewayServer(config util.Config, store db.Store) {
+	server, err := gapi.NewServer(config, store)
+	if err != nil {
+		log.Fatal("cannot create server", err)
+	}
+	jsonOption := runtime.WithMarshalerOption(runtime.MIMEWildcard, &runtime.JSONPb{
+		MarshalOptions: protojson.MarshalOptions{
+			UseProtoNames: true,
+		},
+		UnmarshalOptions: protojson.UnmarshalOptions{
+			DiscardUnknown: true,
+		},
+	})
+	grpcMux := runtime.NewServeMux(jsonOption)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	err = pb.RegisterSimpleBankHandlerServer(ctx, grpcMux, server)
+	if err != nil {
+		log.Fatal("cannot register handler server")
+	}
+	mux := http.NewServeMux()
+	mux.Handle("/", grpcMux)
+
+	listener, err := net.Listen("tcp", config.HTTPServerAddress)
+
+	if err != nil {
+		log.Fatal("cannot create listener:", err)
+	}
+	log.Printf("start HTTP gateway server at %s", listener.Addr().String())
+	err = http.Serve(listener, mux)
+	if err != nil {
+		log.Fatal("cannot start HTTP gateway server", err)
 	}
 
 }
